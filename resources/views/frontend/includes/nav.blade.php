aa6b8fa173df7849d2e320435a1f19b0840df386
Absolom Mugwagwa <absmugz09@gmail.com>
2017-05-11 14:57:12 +0000

    doing the frontend intergration

--- resources/views/frontend/includes/nav.blade.php
+++ resources/views/frontend/includes/nav.blade.php
@@ -1,59 +1,142 @@
-<nav class="navbar navbar-default">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#frontend-navbar-collapse">
-                <span class="sr-only">{{ trans('labels.general.toggle_navigation') }}</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
+<!-- HEADER -->
+<header id="pageTop" class="header">
+
+      <!-- TOP INFO BAR -->
+      <div class="top-info-bar">
+        <div class="container">
+          <div class="top-bar-right">
+            <ul class="list-inline">
+              <li><a href=""><i class="fa fa-envelope" aria-hidden="true"></i> info@yourdomain.com</a></li>
+              <li><span><i class="fa fa-phone" aria-hidden="true"></i>+1 234 567 8900</span></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <!-- NAVBAR -->
+      <nav id="menuBar" class="navbar navbar-default lightHeader" role="navigation">
+        <div class="container">
+
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
             </button>
+            <a class="navbar-brand" href="index.html"><img src="img/logo.png" alt="logo"></a>
+          </div>
 
-            {{ link_to_route('frontend.index', app_name(), [], ['class' => 'navbar-brand']) }}
-        </div><!--navbar-header-->
-
-        <div class="collapse navbar-collapse" id="frontend-navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li>{{ link_to_route('frontend.macros', trans('navs.frontend.macros'), [], ['class' => active_class(Active::checkRoute('frontend.macros')) ]) }}</li>
-            </ul>
-
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse navbar-ex1-collapse">
             <ul class="nav navbar-nav navbar-right">
-                @if (config('locale.status') && count(config('locale.languages')) > 1)
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            {{ trans('menus.language-picker.language') }}
-                            <span class="caret"></span>
-                        </a>
-
-                        @include('includes.partials.lang')
-                    </li>
-                @endif
-
-                @if ($logged_in_user)
-                    <li>{{ link_to_route('frontend.user.dashboard', trans('navs.frontend.dashboard'), [], ['class' => active_class(Active::checkRoute('frontend.user.dashboard')) ]) }}</li>
-                @endif
-
-                @if (! $logged_in_user)
-                    <li>{{ link_to_route('frontend.auth.login', trans('navs.frontend.login'), [], ['class' => active_class(Active::checkRoute('frontend.auth.login')) ]) }}</li>
-
-                    @if (config('access.users.registration'))
-                        <li>{{ link_to_route('frontend.auth.register', trans('navs.frontend.register'), [], ['class' => active_class(Active::checkRoute('frontend.auth.register')) ]) }}</li>
-                    @endif
-                @else
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            {{ $logged_in_user->name }} <span class="caret"></span>
-                        </a>
-
-                        <ul class="dropdown-menu" role="menu">
-                            @permission('view-backend')
-                                <li>{{ link_to_route('admin.dashboard', trans('navs.frontend.user.administration')) }}</li>
-                            @endauth
-                            <li>{{ link_to_route('frontend.user.account', trans('navs.frontend.user.account'), [], ['class' => active_class(Active::checkRoute('frontend.user.account')) ]) }}</li>
-                            <li>{{ link_to_route('frontend.auth.logout', trans('navs.general.logout')) }}</li>
-                        </ul>
-                    </li>
-                @endif
+              <li class="active">
+                <a href="index.html">Home</a>
+              </li>
+              <li class=" dropdown singleDrop">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Services</a>
+                <ul class="dropdown-menu dropdown-menu-left">
+                  <li><a href="service.html">Services</a></li>
+                  <li><a href="single-service.html">Single Services</a></li>
+                </ul>
+              </li>
+              <li class=" dropdown megaDropMenu">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Store</a>
+                <ul class="row dropdown-menu">
+                  <li class="col-sm-3 col-xs-12">
+                    <ul class="list-unstyled">
+                      <li><a href="product-right-sidebar.html">Product Right Sidebar</a></li>
+                      <li><a href="product-left-sidebar.html">Product Left Sidebar</a></li>
+                      <li><a href="product-3col.html">Product 3 Col</a></li>
+                      <li><a href="product-4col.html">Product 4 Col</a></li>
+                      <li><a href="single-product.html">Single product</a></li>
+                    </ul>
+                  </li>
+                  <li class="col-sm-3 col-xs-12">
+                    <ul class="list-unstyled">
+                      <li><a href="user-dashboard.html">User Dashboard</a></li>
+                      <li><a href="user-profile.html">User Profile</a></li>
+                      <li><a href="address.html">Address</a></li>
+                      <li><a href="all-order.html">All Order</a></li>
+                      <li><a href="wishlist.html">Wishlist</a></li>
+                    </ul>
+                  </li>
+                  <li class="col-sm-3 col-xs-12">
+                    <ul class="list-unstyled">
+                      <li><a href="cart.html">Cart</a></li>
+                      <li><a href="checkout.html">Checkout</a></li>
+                      <li><a href="success.html">Confirmation</a></li>
+                      <li><a href="pricing.html">Price table</a></li>
+                    </ul>
+                  </li>
+                  <li class="col-sm-3 col-xs-12 mega-img">
+                    <a href="#"><img src="http://placehold.it/326x218" alt="logo"></a>
+                  </li>
+                </ul>
+              </li>
+              <li class=" dropdown singleDrop">
+                <a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages</a>
+                <ul class="dropdown-menu dropdown-menu-left">
+                  <li><a href="about.html">About Us</a></li>
+                  <li><a href="contact.html">Contact Us</a></li>
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Team</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="member.html">Our Team</a></li>
+                      <li><a href="single-member-profile.html">Single Team</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Gallery</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="gallery-v1.html">Gallery 3 Col</a></li>
+                      <li><a href="gallery-v2.html">Gallery 4 Col</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="login-signup.html">Login SignUp</a></li>
+                  <li><a href="404.html">404 Not Found</a></li>
+                  <li><a href="coming-soon.html">Coming Soon</a></li>
+                </ul>
+              </li>
+              <li class=" dropdown singleDrop">
+                <a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Blog</a>
+                <ul class="dropdown-menu dropdown-menu-right">
+                  <li class="dropdown dropdown-submenu">
+                    <a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown">Blog</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="blog-right-sidebar.html">Right Sidebar</a></li>
+                      <li><a href="blog-left-sidebar.html">Left Sidebar</a></li>
+                      <li><a href="blog-fullwidth.html">Fullwidth</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown dropdown-submenu">
+                    <a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown">Single Blog</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="blog-single-right-sidebar.html">Right Sidebar</a></li>
+                      <li><a href="blog-single-left-sidebar.html">Left Sidebar</a></li>
+                      <li><a href="blog-single-fullwidth.html">Fullwidth</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+              <li class=" dropdown singleDrop">
+                <a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Components</a>
+                <ul class="dropdown-menu">
+                  <li><a href="tabs-pagination.html">Tab & Pagination</a></li>
+                  <li><a href="accrodion.html">Accrodions</a></li>
+                  <li><a href="buttons-alerts.html">Buttons & Alerts</a></li>
+                </ul>
+              </li>
             </ul>
-        </div><!--navbar-collapse-->
-    </div><!--container-->
-</nav>
+          </div>
+
+          <!-- Cart button -->
+          <div class="cart_btn">
+            <a href="cart.html"><i class="fa fa-shopping-basket" aria-hidden="true"></i><span class="badge">0</span></a>
+          </div>
+
+        </div>
+      </nav>
+    </header>
+      <!-- HEADER -->
